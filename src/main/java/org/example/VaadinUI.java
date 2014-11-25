beeea12a6404f430babf17e71184beeec5cc022a
Matti Tahvonen <matti@vaadin.com>
2014-11-25 10:40:27 +0200

    ei varmaan toimi glassfishissä

--- src/main/java/org/example/VaadinUI.java
+++ src/main/java/org/example/VaadinUI.java
@@ -5,7 +5,6 @@ import com.vaadin.cdi.CDIUI;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import java.lang.reflect.InvocationTargetException;
@@ -15,6 +14,7 @@ import javax.inject.Inject;
 import org.apache.commons.beanutils.BeanUtils;
 import org.example.backend.PhoneBookEntry;
 import org.example.backend.PhoneBookService;
+import org.vaadin.cdiviewmenu.ViewMenuUI;
 import org.vaadin.maddon.button.MButton;
 import org.vaadin.maddon.fields.MTable;
 import org.vaadin.maddon.fields.MTextField;
@@ -36,126 +36,6 @@ import org.vaadin.maddon.layouts.MVerticalLayout;
  */
 @CDIUI("")
 @Theme("valo")
-public class VaadinUI extends UI {
-
-    @Inject
-    PhoneBookService service;
-
-    @Inject // With Vaadin CDI one can also inject basic ui components
-    PhoneBookEntryForm form;
-
-    // Instantiate and configure a Table to list PhoneBookEntries
-    MTable<PhoneBookEntry> entryList = new MTable<>(PhoneBookEntry.class)
-            .withHeight("450px")
-            .withFullWidth()
-            .withProperties("name", "number")
-            .withColumnHeaders("Name", "Phone number");
-
-    // Instanticate buttons, hook directly to listener methods in this class
-    Button addNew = new MButton(FontAwesome.PLUS, this::addNew);
-    Button delete = new MButton(FontAwesome.TRASH_O, this::deleteSelected);
-    TextField filter = new MTextField().withInputPrompt("filter...");
-
-    @Override
-    protected void init(VaadinRequest request) {
-
-        // Add some event listners, e.g. to hook filter input to actually 
-        // filter the displayed entries
-        filter.addTextChangeListener(e -> {
-            listEntries(e.getText());
-        });
-        entryList.addMValueChangeListener(this::entrySelected);
-        form.setSavedHandler(this::entrySaved);
-        form.setResetHandler(this::entryEditCanceled);
-
-        setContent(
-                new MVerticalLayout(
-                        new Header("PhoneBook"),
-                        new MHorizontalLayout(addNew, delete, filter),
-                        new MHorizontalLayout(entryList, form)
-                )
-        );
-
-        // List all entries and select first entry in the list
-        listEntries();
-        entryList.setValue(entryList.firstItemId());
-    }
-
-    private void addNew(Button.ClickEvent e) {
-        PhoneBookEntry entry = new PhoneBookEntry();
-        service.save(entry);
-        listEntries();
-        entryList.setValue(entry);
-    }
-
-    private void deleteSelected(Button.ClickEvent e) {
-        service.delete(entryList.getValue());
-        listEntries();
-        entryList.setValue(null);
-    }
-
-    private void listEntries(String filter) {
-        entryList.setBeans(service.getEntries(filter));
-    }
-
-    private void listEntries() {
-        listEntries(filter.getValue());
-    }
-
-    public void entryEditCanceled(PhoneBookEntry entry) {
-        editEntry(entryList.getValue());
-    }
-
-    public void entrySelected(MValueChangeEvent<PhoneBookEntry> event) {
-        editEntry(event.getValue());
-    }
-
-    /**
-     * Assigns the given entry to form for editing.
-     *
-     * @param entry
-     */
-    private void editEntry(final PhoneBookEntry entry) {
-        if (entry == null) {
-            form.setVisible(false);
-            delete.setEnabled(false);
-        } else {
-            delete.setEnabled(true);
-            try {
-                // Example "DB" works with "attached entities", use a clone for 
-                // editing to make changes "buffered". Vaadin also has buffering
-                // on field level that you might find handy sometimes.
-                // In typical applications using e.g. JPA this step is not relevant
-                // as you will be working with a "detached entity"
-                PhoneBookEntry clone = (PhoneBookEntry) BeanUtils.cloneBean(
-                        entry);
-                form.setEntity(clone);
-                form.focusFirst();
-            } catch (IllegalAccessException | InstantiationException | InvocationTargetException | NoSuchMethodException ex) {
-                Logger.getLogger(VaadinUI.class.getName()).log(Level.SEVERE,
-                        null,
-                        ex);
-            }
-        }
-    }
-
-    public void entrySaved(PhoneBookEntry clone) {
-        PhoneBookEntry value = entryList.getValue();
-        try {
-            // Copy the saved state from the clone to "attached entity"
-            // In e.g. typical JPA app this step is not relevant as you'd be
-            // working with detached entities
-            BeanUtils.copyProperties(value, clone);
-            // "save" the modified entity with service call
-            service.save(value);
-            // deselect the entity
-            entryList.setValue(null);
-            // refresh list
-            listEntries();
-        } catch (IllegalAccessException | InvocationTargetException ex) {
-            Logger.getLogger(VaadinUI.class.getName()).log(Level.SEVERE, null,
-                    ex);
-        }
-    }
+public class VaadinUI extends ViewMenuUI {
 
 }
---
+++ src/main/java/org/vaadin/cdiviewmenu/ViewMenu.java
@@ -0,0 +1,319 @@
+package org.vaadin.cdiviewmenu;
+
+import com.vaadin.annotations.Title;
+import com.vaadin.cdi.CDIView;
+import com.vaadin.cdi.UIScoped;
+import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.Resource;
+import com.vaadin.ui.*;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.themes.ValoTheme;
+import org.vaadin.maddon.button.MButton;
+import org.vaadin.maddon.label.Header;
+import org.vaadin.maddon.layouts.MHorizontalLayout;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.util.AnnotationLiteral;
+import javax.inject.Inject;
+import java.util.*;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.mutable.MutableObject;
+
+/**
+ * A helper to automatically create a menu from available Vaadin CDI view.
+ * Listed views should be annotated with ViewMenuItem annotation to be listed
+ * here, there you can also set icon, caption etc.
+ *
+ * You'll probably want something more sophisticated in your app, but this might
+ * be handy prototyping small CRUD apps.
+ *
+ * By default the menu uses Valo themes responsive layout rules, but those can
+ * easily be overridden.
+ *
+ */
+@UIScoped
+public class ViewMenu extends CssLayout {
+
+    @Inject
+    BeanManager beanManager;
+
+    private Header header = new Header(null).setHeaderLevel(3);
+
+    private Button selectedButton;
+
+    private HashMap<String, Button> nameToButton = new HashMap<>();
+    private Button active;
+    private Component secondaryComponent;
+    private CssLayout items;
+
+    public List<Bean<?>> getAvailableViews() {
+        Set<Bean<?>> all = beanManager.getBeans(View.class,
+                new AnnotationLiteral<Any>() {
+                });
+
+        final ArrayList<Bean<?>> list = new ArrayList<>();
+        for (Bean<?> bean : all) {
+
+            Class<?> beanClass = bean.getBeanClass();
+
+            ViewMenuItem annotation = beanClass.
+                    getAnnotation(ViewMenuItem.class);
+            if (annotation == null || annotation.enabled()) {
+                list.add(bean);
+            }
+        }
+
+        Collections.sort(list, new Comparator<Bean<?>>() {
+
+            @Override
+            public int compare(Bean<?> o1, Bean<?> o2) {
+                ViewMenuItem a1 = o1.getBeanClass().
+                        getAnnotation(ViewMenuItem.class);
+                ViewMenuItem a2 = o2.getBeanClass().
+                        getAnnotation(ViewMenuItem.class);
+                if (a1.order() == a2.order()) {
+                    return a1.title().compareTo(a2.title());
+                } else {
+                    return a1.order() - a2.order();
+                }
+            }
+        });
+
+        // TODO check if accessible for current user
+        // TODO check if accessible for current user
+        return list;
+    }
+
+    @PostConstruct
+    void init() {
+        createHeader();
+
+        final Button showMenu = new Button("Menu", new ClickListener() {
+            @Override
+            public void buttonClick(final ClickEvent event) {
+                if (getStyleName().contains("valo-menu-visible")) {
+                    removeStyleName("valo-menu-visible");
+                } else {
+                    addStyleName("valo-menu-visible");
+                }
+            }
+        });
+        showMenu.addStyleName(ValoTheme.BUTTON_PRIMARY);
+        showMenu.addStyleName(ValoTheme.BUTTON_SMALL);
+        showMenu.addStyleName("valo-menu-toggle");
+        showMenu.setIcon(FontAwesome.LIST);
+        addComponent(showMenu);
+
+        items = new CssLayout(getAsLinkButtons(getAvailableViews()));
+        items.setPrimaryStyleName("valo-menuitems");
+        addComponent(items);
+
+        addAttachListener(new AttachListener() {
+            @Override
+            public void attach(AttachEvent event) {
+                if (getMenuTitle() == null) {
+                    setMenuTitle(detectMenuTitle());
+                }
+                Navigator navigator = UI.getCurrent().getNavigator();
+                if (navigator != null) {
+                    String state = navigator.getState();
+                    if (state == null) {
+                        state = "";
+                    }
+                    Button b = nameToButton.get(state);
+                    if (b != null) {
+                        emphasisAsSelected(b);
+                    }
+                }
+            }
+        }
+        );
+    }
+
+    protected void createHeader() {
+        setPrimaryStyleName("valo-menu");
+        addStyleName("valo-menu-part");
+        MHorizontalLayout headercontent = new MHorizontalLayout(
+                header).withMargin(false).
+                alignAll(Alignment.MIDDLE_CENTER);
+        headercontent.setStyleName("valo-menu-title");
+        addComponent(headercontent);
+    }
+
+    private Component[] getAsLinkButtons(List<Bean<?>> availableViews) {
+
+        Collections.sort(availableViews, new Comparator<Bean<?>>() {
+
+            @Override
+            public int compare(Bean<?> o1, Bean<?> o2) {
+                return 0;
+            }
+        });
+
+        ArrayList<Button> buttons = new ArrayList<>();
+        for (Bean<?> viewBean : availableViews) {
+
+            Class<?> beanClass = viewBean.getBeanClass();
+
+            ViewMenuItem annotation = beanClass.
+                    getAnnotation(ViewMenuItem.class
+                    );
+            if (annotation
+                    != null && !annotation.enabled()) {
+                continue;
+            }
+
+            if (beanClass.getAnnotation(CDIView.class
+            ) != null) {
+                MButton button = getButtonFor(beanClass);
+                CDIView view = beanClass.getAnnotation(CDIView.class);
+
+                nameToButton.put(view.value(), button);
+                buttons.add(button);
+            }
+        }
+
+        return buttons.toArray(new Button[0]);
+    }
+
+    protected MButton getButtonFor(final Class<?> beanClass) {
+        final MButton button = new MButton(getNameFor(beanClass));
+        button.setPrimaryStyleName("valo-menu-item");
+        button.setIcon(getIconFor(beanClass));
+        button.addClickListener(new ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                navigateTo(beanClass);
+            }
+        });
+        return button;
+    }
+
+    protected void emphasisAsSelected(Button button) {
+        if (selectedButton != null) {
+            selectedButton.removeStyleName("selected");
+        }
+        button.addStyleName("selected");
+        selectedButton = button;
+    }
+
+    protected Resource getIconFor(Class<?> viewType) {
+        ViewMenuItem annotation = viewType.getAnnotation(ViewMenuItem.class);
+        if(annotation == null) {
+            return FontAwesome.FILE;
+        }
+        return annotation.icon();
+    }
+
+    protected String getNameFor(Class<?> viewType) {
+        ViewMenuItem annotation = viewType.getAnnotation(ViewMenuItem.class);
+        if (annotation != null && !annotation.title().isEmpty()) {
+            return annotation.title();
+        }
+        String simpleName = viewType.getSimpleName();
+        // remove trailing view
+        simpleName = simpleName.replaceAll("View$", "");
+        // decamelcase
+        simpleName = StringUtils.join(StringUtils.splitByCharacterTypeCamelCase(
+                simpleName), " ");
+        return simpleName;
+    }
+
+    public void setActive(String viewId) {
+        if (active != null) {
+            active.setEnabled(true);
+        }
+        active = nameToButton.get(viewId);
+        if (active != null) {
+            active.setEnabled(false);
+        }
+    }
+
+    public String getMenuTitle() {
+        return header.getText();
+    }
+
+    public void setMenuTitle(String menuTitle) {
+        this.header.setText(menuTitle);
+    }
+
+    private String detectMenuTitle() {
+        // try to dig a sane default from Title annotation in UI or class name
+        final Class<? extends UI> uiClass = getUI().getClass();
+        Title title = uiClass.getAnnotation(Title.class
+        );
+        if (title
+                != null) {
+            return title.value();
+        } else {
+            String simpleName = uiClass.getSimpleName();
+            return simpleName.replaceAll("UI", "");
+        }
+    }
+
+    public View navigateTo(final Class<?> viewClass) {
+        CDIView cdiview = viewClass.getAnnotation(CDIView.class);
+        final String viewId = cdiview.value();
+        return navigateTo(viewId);
+    }
+
+    public View navigateTo(final String viewId) {
+        removeStyleName("valo-menu-visible");
+        Button button = nameToButton.get(viewId);
+        if (button != null) {
+            final Navigator navigator = UI.getCurrent().getNavigator();
+
+            final MutableObject<View> view = new MutableObject<>();
+
+            ViewChangeListener l = new ViewChangeListener() {
+
+                @Override
+                public boolean beforeViewChange(
+                        ViewChangeListener.ViewChangeEvent event) {
+                    return true;
+                }
+
+                @Override
+                public void afterViewChange(
+                        ViewChangeListener.ViewChangeEvent event) {
+                    view.setValue(event.getNewView());
+                }
+            };
+
+            navigator.addViewChangeListener(l);
+            navigator.navigateTo(viewId);
+            navigator.removeViewChangeListener(l);
+            emphasisAsSelected(button);
+            return view.getValue();
+        }
+        return null;
+    }
+
+    public void setSecondaryComponent(Component component) {
+        if (secondaryComponent != component) {
+            if (secondaryComponent != null) {
+                removeComponent(secondaryComponent);
+            }
+            secondaryComponent = component;
+            addComponent(component, 1);
+        }
+    }
+
+    /**
+     * Adds a custom button to the menu.
+     *
+     * @param button
+     */
+    public void addMenuItem(Button button) {
+        button.setPrimaryStyleName("valo-menu-item");
+        items.addComponent(button);
+    }
+
+}
