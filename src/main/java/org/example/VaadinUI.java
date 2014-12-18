6c5e539c737f2f6bfaf8481c6f536b1e0023c3ff
Matti Tahvonen <matti@vaadin.com>
2014-12-18 16:25:20 +0200

    GlassFish compatible version of example

--- src/main/java/org/example/VaadinUI.java
+++ src/main/java/org/example/VaadinUI.java
@@ -2,26 +2,7 @@ package org.example;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.cdi.CDIUI;
-import com.vaadin.server.FontAwesome;
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import java.lang.reflect.InvocationTargetException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.inject.Inject;
-import org.apache.commons.beanutils.BeanUtils;
-import org.example.backend.PhoneBookEntry;
-import org.example.backend.PhoneBookService;
 import org.vaadin.cdiviewmenu.ViewMenuUI;
-import org.vaadin.maddon.button.MButton;
-import org.vaadin.maddon.fields.MTable;
-import org.vaadin.maddon.fields.MTextField;
-import org.vaadin.maddon.fields.MValueChangeEvent;
-import org.vaadin.maddon.label.Header;
-import org.vaadin.maddon.layouts.MHorizontalLayout;
-import org.vaadin.maddon.layouts.MVerticalLayout;
 
 /**
  * This is a small tutorial application for Vaadin. It also uses Vaadin CDI (so
